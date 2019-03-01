3bd72068b6866b1a8bbcdc7c7e3b8796aa49def8
Ihar <petrushen@yahoo.com>
2019-03-01 20:43:22 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,41 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+  let tr = 0;
+
+for(let j = 0; j < preferences.length; j++) {
+
+  if(LinkedList(j) && preferences[j] !==j+1
+  && preferences[preferences[preferences[j]-1]-1] === j+1
+  )
+  {
+
+    tr++;
+
+  }
+  function LinkedList(j){
+    var head = preferences[j];
+  
+    var Node = function(j){
+      this.element = preferences[j];
+      this.next = preferences[element - 1];
+    };
+
+    this.nodes = function(j){
+      firstNode = head;
+      secondNode = firstNode.next;
+      thirdNode = secondNode.next;
+      if(secondNode && thirdNode && firstNode){
+    
+        return true;
+      }
+    }
+    if(this.nodes){
+      return true;
+    }
+  
+  }
+
+}
+return Math.floor(tr/3);
+}
